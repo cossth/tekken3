1b3178bf4ec775416ee96f8c05d64b78990cbc79
Shubham Sharma <cossth@gmail.com>
2019-12-10 23:37:34 +0530

    fIRST cOMMIT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Character } from "./models/character";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'tekken3';
+  characters: Character[] = [
+    {
+      name: "Anna",
+      image:
+        "https://giantbomb1.cbsistatic.com/uploads/square_small/1/12541/1518875-anna_main_image.png"
+    },
+    {
+      name: "Dr. Boskonovitch",
+      image:
+        "https://giantbomb1.cbsistatic.com/uploads/square_small/3/33745/2333633-ttt2___boskonovitch.jpg"
+    }
+  ];
 }
